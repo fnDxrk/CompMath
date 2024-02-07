72286d3e0517c7fbdd5a06a299de4ea8c2e45f69
Phlipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-02-07 23:12:15 +0700

    Method Gaussa

--- Gauss/src/main.cpp
+++ Gauss/src/main.cpp
@@ -1,22 +1,52 @@
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-class Matrix {
-private:
-    int rows;
-    int cols;
-    vector<vector<int>> data;
+// Вывод матрицы
+void printMatrix(vector<vector<double>> matrix, int size)
+{
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size + 1; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 
-public:
-    Matrix(int rows, int cols);
-};
+    cout << endl;
+}
+
+// Метод Гаусса
+void methodGauss(vector<vector<double>>& matrix, int size, bool value)
+{
+    // Перестановка строк
+    if (value == true) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i; j < size - 1; j++) {
+                if (abs(matrix[i][i]) < abs(matrix[j][i])) {
+                    swap(matrix[i], matrix[j]);
+                }
+            }
+        }
+    }
+
+    // Прямой ход
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            double attitude = matrix[j][i] / matrix[i][i];
+            for (int k = 0; k < size + 1; k++) {
+                matrix[j][k] = matrix[j][k] - attitude * matrix[i][k];
+            }
+        }
+    }
+}
 
 int main()
 {
+    // Файл
     ifstream file("../input.txt");
 
     // Проверка на открытие файла
@@ -25,37 +55,52 @@ int main()
         return 1;
     }
 
+    // Строка для чтения
     string line;
-    int count = 0, rows = 0, cols = 0;
 
-    // Кол-во строк и кол-во цифр
+    // Размер матрицы
+    int size = 0;
+
+    // Кол-во строк
     while (getline(file, line)) {
-        ++rows;
-        for (char c : line) {
-            if (!isspace(c))
-                count++;
-        }
+        ++size;
     }
-
-    // Кол-во столбцов
-    cols = count % rows != 0 ? count / rows + 1 : count / rows;
 
     // Перевод курсора в начало
     file.clear();
     file.seekg(0);
 
-    vector<vector<int>> matrix(rows, vector<int>(cols));
+    // Вектор матрицы
+    vector<vector<double>> matrix(size, vector<double>(size + 1));
+    // Вектор X'ов
+    vector<double> x(size);
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    // Заполнение матрицы из файла
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size + 1; j++) {
             file >> matrix[i][j];
         }
     }
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << matrix[i][j] << " ";
+    printMatrix(matrix, size);
+
+    methodGauss(matrix, size, true);
+
+    printMatrix(matrix, size);
+
+    // Последний X
+    x[size - 1] = matrix[size - 1][size] / matrix[size - 1][size - 1];
+
+    // Оставшиеся x
+    for (int i = size - 2; i >= 0; i--) {
+        x[i] = matrix[i][size];
+        for (int j = i + 1; j < size; j++) {
+            x[i] -= matrix[i][j] * x[j];
         }
-        cout << "\n" << endl;
+        x[i] = x[i] / matrix[i][i];
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << "x[" << i + 1 << "] = " << setprecision(2) << fixed << x[i] << endl;
     }
 }
